09587c7a67e5113eca0ea9325d50e02f70155d4f
alex9r <89540509+alex9r@users.noreply.github.com>
2025-03-02 21:46:11 -0500

    seed pouch design

---
+++ mastermind/Colour.swift
@@ -0,0 +1,15 @@
+//
+//  Colour.swift
+//  mastermind
+//
+//  Created by Alexandra Roszczenko on 2025-03-02.
+//
+
+import SwiftUI
+
+extension Color {
+    init(r: Double, g: Double, b: Double) {
+        self.init(red: r, green: g, blue: b)
+    }
+}
+
--- mastermind/seedPouchPopUp.swift
+++ mastermind/seedPouchPopUp.swift
@@ -8,30 +8,51 @@
 import SwiftUI
 
 struct SeedPouchPopupView: View {
+    // Binding to the state from the parent view
     @Binding var isPresented: Bool
-    var body: some View {
-        VStack {
-            Text("You opened the Seed Pouch!")
-                .font(.title)
-                .padding()
 
-            Button(action: {
-                // Close the pop-up by setting isPresented to false
-                isPresented = false
-            }) {
-                Text("Close")
-                    .font(.headline)
+    var body: some View {
+        ZStack {
+//            RadialGradient(
+//                gradient: Gradient(colors: [Color.brown.opacity(0.5), Color.brown.opacity(0.7)]),
+//                center: .center,
+//                startRadius: 10,
+//                endRadius: 300
+//            )
+//            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("seed pouch")
+                    .font(.title)
                     .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                .padding(.top, 20)
             }
-            .padding(.top, 20)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 10)
+
+            // Close button at the top-right corner
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        isPresented = false
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 30))
+                            .foregroundColor(Color(r: 52/255, g: 24/255, b: 16/255))
+                            .padding()
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            .background(Color(r: 95/255, g: 64/255, b: 55/255)) // Background color for the popup content
+            .cornerRadius(20) // Optional: to round the corners of the popup
+            .shadow(radius: 10) // Optional: add a shadow for better visibility
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white)
-        .cornerRadius(20)
-        .shadow(radius: 10)
     }
 }
+
 
